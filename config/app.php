59eabb088f57649b76b2553c103df8340411dc15
proclasse54 <tmp@cque.fr>
2026-04-20 10:12:41 +0200

    Ajout .gitignore

--- config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config/app.php
-return [
-    'name'     => 'ProClasse',
-    'base_url' => 'https://proclasse.fr',
-    'debug'    => false,
-    'timezone' => 'Europe/Paris',
-];
--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config/database.php
-return [
-    'host'     => 'localhost',
-    'dbname'   => 'proclasse',
-    'user'     => 'proclasse',
-    'password' => 'proclasse',
-    'charset'  => 'utf8mb4',
-];
