c14fc49e2ea6d6a0d065337c92177c3d311b5d28
proclasse54 <tmp@cque.fr>
2026-04-24 22:52:34 +0200

    Photos des noms composés.

--- src/Photos.php
+++ src/Photos.php
@@ -20,14 +20,30 @@ function removeAccents(string $str): string {
  * Format fichier : CLASSE.NOM.Prenom.jpg
  */
 function getPhotoUrl(string $classe, string $nom, string $prenom): ?string {
-    $prenomFichier = removeAccents($prenom);
+
+    $classeFichier = nettoyerChaine($classe);
+    $nomFichier    = nettoyerChaine(mb_strtoupper($nom, 'UTF-8'));
+    $prenomFichier = removeAccents(nettoyerChaine($prenom));
     
     // Chemin absolu réel sur le serveur
     $cheminAbsolu = '/var/www/sub-domains/proclasse/public/data/photos_eleves/' 
-                    . "{$classe}.{$nom}.{$prenomFichier}.jpg";
+                    . "{$classeFichier}.{$nomFichier}.{$prenomFichier}.jpg";
     
     // URL publique retournée au navigateur
-    $urlPublique = "/data/photos_eleves/{$classe}.{$nom}.{$prenomFichier}.jpg";
+    $urlPublique = "/data/photos_eleves/{$classeFichier}.{$nomFichier}.{$prenomFichier}.jpg";
     
     return file_exists($cheminAbsolu) ? $urlPublique : null;
+}
+
+function nettoyerChaine(string $str): string
+{
+    if (class_exists('Normalizer')) {
+        $str = Normalizer::normalize($str, Normalizer::FORM_D);
+        $str = preg_replace('/\p{Mn}/u', '', $str);
+    } else {
+        $str = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $str);
+    }
+    // espaces → tirets, caractères spéciaux supprimés
+    $str = preg_replace('/\s+/', '-', trim($str));
+    return preg_replace('/[^a-zA-Z0-9\-]/', '', $str);
 }
